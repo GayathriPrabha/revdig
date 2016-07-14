61fccf6f0f6af3c8531167ead813f79f21d8ba9e
GayathriPrabha <gayathri6996@gmail.com>
2016-07-14 06:44:40 +0530

    Create revdig.c

---
+++ revdig.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n,rev=0;
+printf("Enter the number:");
+scanf("%d",&n);
+while(n!=0)
+{
+rev=rev*10;
+rev=rev+n%10;
+n=n/10;
+}
+printf("the reverse of number is %d",rev);
+}
